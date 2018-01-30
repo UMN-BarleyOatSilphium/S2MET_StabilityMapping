2063ee155c1ab62a238ec0f51723d1718360bf39
neyhartj <neyha001@umn.edu>
2018-01-30 10:34:28 -0600

    Small running edits

--- PhenotypeManipulation/run_marker_by_env.sh
+++ PhenotypeManipulation/run_marker_by_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -l walltime=24:00:00,mem=24gb,nodes=1:ppn=16
-#PBS -N S2_MET_stability_mxe
+#PBS -N S2_MET_stability_mxe_ph
 #PBS -M neyha001@umn.edu
 #PBS -m abe
 #PBS -r n
@@ -13,6 +13,6 @@ cd /panfs/roc/groups/6/smithkp/neyha001/Genomic_Selection/S2MET_Mapping/Phenotyp
 module load R/3.2.0_intel_mkl
 
 # Marker effect by environment computation
-Rscript S2MET_marker_effect_by_env.R HeadingDate
-Rscript S2MET_marker_effect_by_env.R GrainYield
+#Rscript S2MET_marker_effect_by_env.R HeadingDate
+#Rscript S2MET_marker_effect_by_env.R GrainYield
 Rscript S2MET_marker_effect_by_env.R PlantHeight
