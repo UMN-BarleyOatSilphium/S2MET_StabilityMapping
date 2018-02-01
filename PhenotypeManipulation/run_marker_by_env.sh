979946b668e174b16a2b6478ba362ef3d93b3eda
neyhartj <neyhartje@gmail.com>
2018-01-31 21:46:30 -0600

    Edits to bash script

--- PhenotypeManipulation/run_marker_by_env.sh
+++ PhenotypeManipulation/run_marker_by_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#PBS -l walltime=24:00:00,mem=62gb,nodes=1:ppn=24
-#PBS -N S2_MET_stability_mxe
+#PBS -l walltime=24:00:00,mem=22gb,nodes=1:ppn=4
+#PBS -N S2_MET_stability_mar_stab_varcomp
 #PBS -M neyha001@umn.edu
 #PBS -m abe
 #PBS -r n
@@ -15,4 +15,9 @@ module load R/3.2.0_intel_mkl
 # Marker effect by environment computation
 #Rscript S2MET_marker_effect_by_env.R HeadingDate
 #Rscript S2MET_marker_effect_by_env.R GrainYield
-Rscript S2MET_marker_effect_by_env.R PlantHeight
+# Rscript S2MET_marker_effect_by_env.R PlantHeight
+
+Rscript S2MET_marker_stability_varcomp.R HeadingDate
+Rscript S2MET_marker_stability_varcomp.R GrainYield
+Rscript S2MET_marker_stability_varcomp.R PlantHeight
+
